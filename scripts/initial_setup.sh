d082c07baf7f0c61dc6c12ea729d33de65ef3497
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-04-19 10:25:45 -0300

    [REM] deprecated cleanup

--- scripts/initial_setup.sh
+++ scripts/initial_setup.sh
@@ -2,7 +2,6 @@
 DIR="$(cd "$(dirname "$0")" && pwd)"
 $DIR/run_aeroo_docs.sh
 $DIR/run_traefik.sh
-#$DIR/run_cleanup.sh
 $DIR/run_portainer.sh
 # last because it restart networking and docker wont listen
 $DIR/setup_dnsmasq.sh
