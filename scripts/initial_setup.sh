58eddc79f786c616d19eee886036e1349bd72e09
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-10-27 06:59:35 -0300

    [IMP] move aeroo into compose and rename global to traefik

--- scripts/initial_setup.sh
+++ scripts/initial_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker-compose -p global -f global.yml up -d
+docker-compose -p traefik -f traefik.yml up -d
 DIR="$(cd "$(dirname "$0")" && pwd)"
 $DIR/link_volumes.sh
 $DIR/config_prompt.sh
