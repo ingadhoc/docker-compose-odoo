8211dad9c9773101862b80902ebb3411329f4c6d
Iván Todorovich <ivan.todorovich@gmail.com>
2024-08-30 14:19:16 -0300

    [IMP] Run all projects within the same network

--- scripts/initial_setup.sh
+++ scripts/initial_setup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-docker-compose -p traefik -f traefik.yml up -d
+
+# Create adhoc network if it doesn't exist
+if ! docker network inspect adhoc > /dev/null 2>&1; then
+  docker network create --subnet=172.20.0.0/16 adhoc
+fi
+
+# Start traefik
+docker compose -f traefik.yml up -d
+
+# Link volumes
 DIR="$(cd "$(dirname "$0")" && pwd)"
 $DIR/link_volumes.sh
