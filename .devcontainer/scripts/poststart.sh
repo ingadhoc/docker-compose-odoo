c99f89b39ae8100b0e634f95b6aa4565db8d7b0b
Andrés Zacchino <az@adhoc.com.ar>
2025-02-24 14:25:17 -0300

    [FIX] sym links

--- .devcontainer/scripts/poststart.sh
+++ .devcontainer/scripts/poststart.sh
@@ -3,12 +3,17 @@
 # Fix addons paths
 for app in "/home/odoo/custom/repositories/"*; do
     if [[ -d $app ]]; then
-        [[ $app == .* || $app == src* ]] && continue
+        app_name=$(basename $app)
+        [[ $app_name == .* || $app_name == src* ]] && continue
+        echo "App: $app_name"
         for module in "$app/"*; do
             if [[ -d $module ]]; then
-                [[ $module == .* || $module == src* ]] && continue
                 module_name=$(basename $module)
-                ln -sf "$module/" /home/odoo/src/odoo/odoo/addons/$module_name
+                [[ $module_name == .* || $module_name == src* ]] && continue
+                echo "ln -sf $module/ /home/odoo/src/odoo/odoo/addons/$module_name"
+                if [ ! -L /home/odoo/src/odoo/odoo/addons/$module_name ]; then
+                    ln -sf $module/ /home/odoo/src/odoo/odoo/addons/$module_name
+                fi
                 echo "odoo/addons/$module_name" >> /home/odoo/src/odoo/.git/info/exclude
             fi
         done
