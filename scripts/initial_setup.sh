62e5040c9e4397af23aff808a33e33ae9cf41dc0
Andrés Zacchino <az@adhoc.com.ar>
2025-04-22 12:37:20 -0300

    [REM] remove unused files

--- scripts/initial_setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-DIR="$(cd "$(dirname "$0")" && pwd)"
-$DIR/link_volumes.sh
--- scripts/update_environment.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker-compose pull
-docker-compose rm -f
-project=${PWD##*/} && project=${project//-} && docker volume rm ${project}_default
