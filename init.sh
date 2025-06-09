470b10c615b3d55364bb83462c08fe7a343ccc0b
Andrés Zacchino <az@adhoc.inc>
2025-06-09 12:00:21 -0300

    [ADD] master image

--- init.sh
+++ init.sh
@@ -2,31 +2,35 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ODOO_V=$(basename "$SCRIPT_DIR")
 
-# Check if "ODOO_V" is 2 digits
-if [[ "$ODOO_V" =~ ^[0-9]{2}$ ]] && [[ -f "$SCRIPT_DIR/.env" ]]; then
-    echo "Fixing env vars"
-    sed -i "s/^ODOO_MINOR=.*/ODOO_MINOR=$ODOO_V.0.dev/" "$SCRIPT_DIR/.env"
-    sed -i "s/^DOMAIN=.*/DOMAIN=$ODOO_V.odoo.localhost/" "$SCRIPT_DIR/.env"
-    sed -i "s/^ODOO_VERSION=.*/ODOO_VERSION=$ODOO_V/" "$SCRIPT_DIR/.env"
-fi
-
-if [[ "$ODOO_V" == "master" ]]; then
-    echo "Fixing env vars"
-    # TODO: Update the ODOO_MINOR to master.dev when be ready
-    # sed -i "s/^ODOO_MINOR=.*/ODOO_MINOR=$ODOO_V.0.dev/" "$SCRIPT_DIR/.env"
-    sed -i "s/^SERVER_WIDE_MODULES=/# SERVER_WIDE_MODULES=/" "$SCRIPT_DIR/.env"
-    sed -i "s/^IGNORE_SRC_REPOSITORIES=.*$/IGNORE_SRC_REPOSITORIES=True/" "$SCRIPT_DIR/.env"
-    sed -i "s/^DOMAIN=.*/DOMAIN=$ODOO_V.odoo.localhost/" "$SCRIPT_DIR/.env"
-    sed -i "s/^ODOO_VERSION=.*/ODOO_VERSION=99/" "$SCRIPT_DIR/.env"
-
-    sed -i "s|/home/odoo/custom/repositories|/home/odoo/custom|g" "$SCRIPT_DIR/.devcontainer/devcontainer.json"
-    sed -i "s|\"AD_DEV_MODE\": \"NORMAL\"|\"AD_DEV_MODE\": \"MASTER\"|g" "$SCRIPT_DIR/.devcontainer/devcontainer.json"
-fi
-
 if [[ -f "$SCRIPT_DIR/.env" ]]; then
+    # Check if "ODOO_V" is 2 digits or "master"
+    if [[ "$ODOO_V" =~ ^[0-9]{2}$ ]] || [[ "$ODOO_V" == "master" ]]; then
+        echo "Fixing env vars"
+        sed -i "s/^DOMAIN=.*/DOMAIN=$ODOO_V.odoo.localhost/" "$SCRIPT_DIR/.env"
+        sed -i "s/^ODOO_VERSION=.*/ODOO_VERSION=$ODOO_V/" "$SCRIPT_DIR/.env"
+    fi
+
+    if [[ "$ODOO_V" =~ ^[0-9]{2}$ ]]; then
+        sed -i "s/^ODOO_MINOR=.*/ODOO_MINOR=$ODOO_V.0.dev/" "$SCRIPT_DIR/.env"
+    fi
+
+    if [[ "$ODOO_V" == "master" ]]; then
+        sed -i "s/^ODOO_MINOR=.*/ODOO_MINOR=$ODOO_V.dev/" "$SCRIPT_DIR/.env"
+        sed -i "s/^SERVER_WIDE_MODULES=/# SERVER_WIDE_MODULES=/" "$SCRIPT_DIR/.env"
+        sed -i "s/^IGNORE_SRC_REPOSITORIES=.*$/IGNORE_SRC_REPOSITORIES=True/" "$SCRIPT_DIR/.env"
+        sed -i "s|/home/odoo/custom/repositories|/home/odoo/custom|g" "$SCRIPT_DIR/.devcontainer/devcontainer.json"
+        sed -i "s|\"AD_DEV_MODE\": \"NORMAL\"|\"AD_DEV_MODE\": \"MASTER\"|g" "$SCRIPT_DIR/.devcontainer/devcontainer.json"
+    fi
+
+    if [[ "$ODOO_V" =~ ^[0-9]{2}$ ]] && [[ "$ODOO_V" -le "17" ]]; then
+        echo "Disabling format on save for Odoo $ODOO_V"
+        sed -i "s|\"editor.formatOnSave\": true,|\"editor.formatOnSave\": false,|g" "$SCRIPT_DIR/.devcontainer/devcontainer.json"
+    fi
+
     echo "Pull latest image"
     source "$SCRIPT_DIR/.env"
     docker pull ${ODOO_IMAGE}:${ODOO_MINOR}
+
 fi
 
 echo "Binding directory"
