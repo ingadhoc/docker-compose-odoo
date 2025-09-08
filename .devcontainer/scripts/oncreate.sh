a1cbcfba6eb0d31684f996c6f02aa8c4f0a0f772
Andrés Zacchino <az@adhoc.inc>
2025-09-08 11:21:19 -0300

    [FIX] typo

--- .devcontainer/scripts/oncreate.sh
+++ .devcontainer/scripts/oncreate.sh
@@ -7,11 +7,12 @@ rm -f /home/odoo/.gitconfig
 
 # We bring the addons from src to custom/repositories/src and leave them as symbolic links
 # so that Odoo can detect them as addons and we can use them in the container
-rm -f /home/odoo/custom/repositories/src
+rm -rf /home/odoo/custom/repositories/src
 mkdir -p /home/odoo/custom/repositories/src
 for app in "/home/odoo/src/"*; do
     if [[ -d $app ]]; then
         module_name=$(basename $app)
+        ln -sf $app /home/odoo/custom/src/$module_name
         echo "Creating symlink for $module_name ln -sf $app /home/odoo/custom/src/$module_name"
     fi
 done
